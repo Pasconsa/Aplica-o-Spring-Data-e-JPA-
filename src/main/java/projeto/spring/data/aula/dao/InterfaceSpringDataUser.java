a8bd3e04bba1b14d8ee67181d0f87885ca66084e
Pasconsa <saulopascon@hotmail.com>
2022-08-11 14:30:34 -0300

    1 Update e delete conditional by Spring

--- src/main/java/projeto/spring/data/aula/dao/InterfaceSpringDataUser.java
+++ src/main/java/projeto/spring/data/aula/dao/InterfaceSpringDataUser.java
@@ -1,11 +1,39 @@
 package projeto.spring.data.aula.dao;
 
+import java.util.List;
+
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import projeto.spring.data.aula.model.UsuarioSpringData;
 
 @Repository
 public interface InterfaceSpringDataUser extends CrudRepository<UsuarioSpringData, Long>{
+	
+	@Query(value = "select p from UsuarioSpringData p where p.nome like %?1%")
+	public List<UsuarioSpringData> buscaPorNomeAsync (String nome);
+	
+	//Consultando Query por parametro
+	@Query(value = "select p from UsuarioSpringData p where p.nome = :paramnome")  //Colocar o JPQL , no caso aqui retorna um objeto
+	public UsuarioSpringData buscaPorNomeParam (@Param("paramnome") String paramnome);
+
+	
+	//Maneira condicional
+	
+	//deletar por nome Query criada que não existe no padrão.
+	@Modifying   //anoto para o banco modificar
+	@Transactional //anoto parastarta uma transação / abaixo comando
+	@Query("delete from UsuarioSpringData u where u.nome = ?1") //delete na tabela usua.. u onde u.nome = parametro que vou passar.
+	public void deletePorNome(String nome);
+	
+	//update condicional dois parametros
+	@Modifying
+	@Transactional
+	@Query("update UsuarioSpringData u set u.email = ?1 where u.nome = ?2") //são dois parametros
+	public void updateEmailPorNome(String email, String nome);
 
 }
--- src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
+++ src/test/java/projeto/spring/data/aula/AppSpringDataTest.java
@@ -1,5 +1,6 @@
 package projeto.spring.data.aula;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.junit.Test;
@@ -18,38 +19,39 @@ public class AppSpringDataTest {
 
 	@Autowired
 	private InterfaceSpringDataUser interfaceStringDataUser;
-	
-	@Test //Cadastrando Banco de dados
+
+	@Test // Cadastrando Banco de dados
 	public void testeInsert() {
-		
+
 		UsuarioSpringData usuarioSpringData = new UsuarioSpringData();
 
 		usuarioSpringData.setEmail("Larissa@hotmail.com");
 		usuarioSpringData.setIdade(3);
 		usuarioSpringData.setLogin("teste 123");
 		usuarioSpringData.setSenha("123");
-		usuarioSpringData.setNome("Lala");
+		usuarioSpringData.setNome("Lucas");
 
-		interfaceStringDataUser.save(usuarioSpringData); //pode fazer varias ações como deletetar , pesquisar
-		
+		interfaceStringDataUser.save(usuarioSpringData); // pode fazer varias ações como deletetar , pesquisar
+
 		System.out.println("Usuario cadastrado -> " + interfaceStringDataUser.count());
 
 	}
-	
-	@Test	//Consulta Banco de dados
+
+	@Test // Consulta Banco de dados
 	public void testeConsulta() {
 
-		Optional<UsuarioSpringData> usuarioSpringData = interfaceStringDataUser.findById(2L); //find retorna um objeto optional
+		Optional<UsuarioSpringData> usuarioSpringData = interfaceStringDataUser.findById(2L); // find retorna um objeto
+																								// optional
 		System.out.println(usuarioSpringData.get().getEmail());
 		System.out.println(usuarioSpringData.get().getIdade());
 		System.out.println(usuarioSpringData.get().getLogin());
 		System.out.println(usuarioSpringData.get().getNome());
 		System.out.println(usuarioSpringData.get().getSenha());
 		System.out.println(usuarioSpringData.get().getId());
-		
+
 	}
-	
-	@Test //atualizar
+
+	@Test // atualizar
 	public void testeUpdate() {
 
 		Optional<UsuarioSpringData> usuarioSpringData = interfaceStringDataUser.findById(3L);
@@ -61,13 +63,50 @@ public class AppSpringDataTest {
 
 		interfaceStringDataUser.save(data);
 	}
-	
-	@Test //Deletar objeto consultado
+
+	@Test // Deletar objeto consultado
 	public void testeDelete() {
 		Optional<UsuarioSpringData> usuarioSpringData = interfaceStringDataUser.findById(6L);
 
 		interfaceStringDataUser.delete(usuarioSpringData.get());
 	}
+
+	@Test // consulta por @query
+	public void testeConsultaNome() {
+		List<UsuarioSpringData> list = interfaceStringDataUser.buscaPorNomeAsync("Saulo");
+
+		for (UsuarioSpringData usuarioSpringData : list) {
+			System.out.println(usuarioSpringData.getEmail());
+			System.out.println(usuarioSpringData.getIdade());
+			System.out.println(usuarioSpringData.getLogin());
+			System.out.println(usuarioSpringData.getNome());
+			System.out.println(usuarioSpringData.getSenha());
+			System.out.println(usuarioSpringData.getId());
+			System.out.println("---------------------------");
+		}
+	}
+
+	@Test // consulta por @query
+	public void testeConsultaNomeParam() {
+		UsuarioSpringData usuarioSpringData = interfaceStringDataUser.buscaPorNomeParam("Lucas");
+
+		System.out.println(usuarioSpringData.getEmail());
+		System.out.println(usuarioSpringData.getIdade());
+		System.out.println(usuarioSpringData.getLogin());
+		System.out.println(usuarioSpringData.getNome());
+		System.out.println(usuarioSpringData.getSenha());
+		System.out.println(usuarioSpringData.getId());
+		System.out.println("---------------------------");
+	}
+
+	@Test // deletar por condição metodo @modify e transaction
+	public void testeDeletePorNome() {
+		interfaceStringDataUser.deletePorNome("Lucas");
+	}
+
+	@Test // atualizar por condição metodo @modify e transaction 2 parametros
+	public void testeUpdateEmailPorNome() {
+		
+		interfaceStringDataUser.updateEmailPorNome("teste email springData@gmail","Lucas");
+	}
 }
-
-
